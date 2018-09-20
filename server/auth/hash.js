4a89f66cd37d87f162c63a5db195ba971ab05ee9
Aimee Coxhead <aimeecoxhead1@gmail.com>
2018-09-20 18:35:36 +1200

    working on password hashing

---
+++ server/auth/hash.js
@@ -0,0 +1,12 @@
+/* const sodium = require('libsodium-wrappers')
+
+module.exports = {
+  generateHash
+}
+
+function generateHash (password) {
+  return sodium.ready.then(() => 
+sodium.crypto_pwhash_str (
+
+))
+} */ // Working on this still
