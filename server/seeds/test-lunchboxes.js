09c397f3547627f2359c7b60eed3cc0c603a6def
tommy-hall <32444466+tommy-hall@users.noreply.github.com>
2018-09-21 11:58:03 +1200

    Update test-lunchboxes.js

--- server/seeds/test-lunchboxes.js
+++ server/seeds/test-lunchboxes.js
@@ -4,12 +4,12 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('lunchboxes').insert([
-        {id: 88801, user_id: 'jamesb', produce: null, grains: null, dairy: null, protein: null, other: null},
-        {id: 88802, user_id: 'laurenh', produce: null, grains: null, dairy: null, protein: null, other: null},
-        {id: 88803, user_id: 'benh', produce: null, grains: null, dairy: null, protein: null, other: null},
-        {id: 88804, user_id: 'angush', produce: null, grains: null, dairy: null, protein: null, other: null},
-        {id: 88805, user_id: 'maddye', produce: null, grains: null, dairy: null, protein: null, other: null},
-        {id: 88806, user_id: 'zainek', produce: null, grains: null, dairy: null, protein: null, other: null}
+        {id: 77701, user_id: 88801, produce: null, grains: null, dairy: null, protein: null, other: null},
+        {id: 77702, user_id: 88802, produce: null, grains: null, dairy: null, protein: null, other: null},
+        {id: 77703, user_id: 88803, produce: null, grains: null, dairy: null, protein: null, other: null},
+        {id: 77704, user_id: 88804, produce: null, grains: null, dairy: null, protein: null, other: null},
+        {id: 77705, user_id: 88805, produce: null, grains: null, dairy: null, protein: null, other: null},
+        {id: 77706, user_id: 88806, produce: null, grains: null, dairy: null, protein: null, other: null}
       ])
     })
 }
