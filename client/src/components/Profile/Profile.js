ab2b0568b4ce3629f81a067b3da45c5586003ad8
James Built <jgbuilt@gmail.com>
2018-09-20 20:33:07 +1200

    added user profile component & disabled console lint in App

---
+++ client/src/components/Profile/Profile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Profile extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Users Lunch Bunch Homepage!</h1>
+        <p>Welcome to your Lunch Bunch home. What would you like to eat today?</p>
+        <button>Build My Lunch</button>
+      </div>
+    )
+  }
+}
