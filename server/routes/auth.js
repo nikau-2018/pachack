6640016b1d1a00ac4f0fe40c1674a3a518362e2b
Rich Churcher <rich.churcher@gmail.com>
2018-09-21 10:21:21 +1200

    Check password hash
    Former-commit-id: 851d7233eb3607e78af970bd135c677e59c7e102

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const {generateHash} = require('../auth/hash')
+const {checkHash, generateHash} = require('../auth/hash')
 
 const router = express.Router()
 
@@ -9,30 +9,38 @@ const {getUser} = require('../db/users')
 router.post('/login', login)
 
 function login (req, res) {
-  const {username, password} = req.body
-  return getUser(username)
-    .then((user) => {
-      generateHash(password).then(hash => {
-        
-      })
-    })
-    .catch((err) => res.json(err))
+  const { username, password } = req.body
 
-/*
-  if (password === 'password') {
-    return res.json({
-      ok: true,
-      user: {
-        id: 1,
-        username,
-        age: 10
+  return getUser(username)
+    .then(user => {
+      if (!user) {
+        return res.status(400).json({
+          ok: false,
+          error: 'That user does not exist.'
+        })
       }
+
+      const { age, hash, id, username } = user
+
+      checkHash(hash, password)
+        .then(ok => {
+          if (!ok) {
+            return res.status(403).json({
+              ok: false,
+              error: 'Password incorrect.'
+            })
+          }
+
+          return res.json({
+            ok: true,
+            user: { age, id, username }
+          })
+        })
     })
-  }
-  res.json({
-    ok: false,
-    error: 'Password incorrect.'
-  }) */
+    .catch(() => res.json({
+      ok: false,
+      error: 'An unknown error occurred.'
+    }))
 }
 
 module.exports = router
