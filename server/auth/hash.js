bdf1e3793b83d528978c9226be0596ef3eddf516
aimee-coxhead <aimeecoxhead1@gmail.com>
2018-09-20 22:50:41 +1200

    working on hashing password

--- server/auth/hash.js
+++ server/auth/hash.js
@@ -1,12 +1,16 @@
-/* const sodium = require('libsodium-wrappers')
+const sodium = require('libsodium-wrappers')
 
 module.exports = {
   generateHash
 }
 
 function generateHash (password) {
-  return sodium.ready.then(() => 
-sodium.crypto_pwhash_str (
-
-))
-} */ // Working on this still
+  // We have to wait for sodium to initialise. sodium.ready is a promise.
+  return sodium.ready.then(() =>
+    sodium.crypto_pwhash_str(
+      password,
+      sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE,
+      sodium.crypto_pwhash_MEMLIMIT_MIN
+    )
+  )
+}
---
+++ server/routes/auth.js
@@ -0,0 +1,34 @@
+const express = require('express')
+
+const {generateHash} = require('../auth/hash')
+
+const router = express.Router()
+
+const userdb = require('./users')
+
+router.post('/login', login)
+
+function login (req, res) {
+  const {username, password} = req.body
+  generateHash(password)
+  .then (hash => getuser(username)
+)
+
+  if (password === 'password') {
+    return res.json({
+      ok: true,
+      user: {
+        id: 1,
+        username,
+        age: 10
+      }
+    })
+  }
+  res.json({
+    ok: false,
+    error: 'Password incorrect.'
+  })
+}
+
+ module.exports = router
+
