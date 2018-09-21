b9a5fcc3f65c8744738b116dad6a3d9e4b823730
Lauren Hart <laurenannehart@gmail.com>
2018-09-21 14:23:45 +1200

    updated seed data for food ingredients
    Former-commit-id: d7ac72c60d72848a4dc5295d5727f05e01db70ac

--- server/seeds/test-foods.js
+++ server/seeds/test-foods.js
@@ -5,10 +5,25 @@ exports.seed = function (knex, Promise) {
       // Inserts seed entries
       return knex('foods').insert([
         {id: 66601, name: 'cucumber', category_id: 1},
-        {id: 66602, name: 'wheat', category_id: 2},
-        {id: 66603, name: 'milk', category_id: 3},
-        {id: 66604, name: 'chicken', category_id: 4},
-        {id: 66605, name: 'chocolate', category_id: 5}
+        {id: 66602, name: 'apple', category_id: 1},
+        {id: 66603, name: 'banana', category_id: 1},
+        {id: 66604, name: 'carrots_hummus', category_id: 1},
+        {id: 66605, name: 'vegie_crisps', category_id: 2},
+        {id: 66606, name: 'rice_crackers', category_id: 2},
+        {id: 66607, name: 'fried_rice', category_id: 2},
+        {id: 66608, name: 'popcorn', category_id: 2},
+        {id: 66609, name: 'milk_biscuit', category_id: 3},
+        {id: 66610, name: 'yoghurt', category_id: 3},
+        {id: 66611, name: 'cheese_stick', category_id: 3},
+        {id: 66612, name: 'rice_pudding', category_id: 3},
+        {id: 66613, name: 'chicken_wrap', category_id: 4},
+        {id: 66614, name: 'egg', category_id: 4},
+        {id: 66615, name: 'salmon_sushi', category_id: 4},
+        {id: 66616, name: 'biltong', category_id: 4},
+        {id: 66617, name: 'snack_balls', category_id: 5},
+        {id: 66618, name: 'almonds', category_id: 5},
+        {id: 66619, name: 'fruit_nuts', category_id: 5},
+        {id: 66620, name: 'muesli_bar', category_id: 5}
       ])
     })
 }
