8c09eaf0ad81cf5d4579e3c9d8d6cd1f186a47bd
James Built <jgbuilt@gmail.com>
2018-09-21 16:10:21 +1200

    added logo and styling
    Former-commit-id: 6cc3861569c9afc6b44de414c8065e8f9b744928

--- client/src/components/SignIn/SignIn.js
+++ client/src/components/SignIn/SignIn.js
@@ -23,18 +23,19 @@ export default class SignIn extends Component {
   render () {
     return (
       <div>
-        <p>Username: Amy<input className='username'
+        <h3>Log In</h3>
+        <p>Username: <input className='username form-control'
           type='text'
           name='username'
           value={this.state.username}
           onChange={this.handleChange}/>
         </p>
-        <p>Password: <input className='password'
+        <p>Password: <input className='password form-control'
           type='text'
           name='password'
           value={this.state.password}
           onChange={this.handleChange}/>
-        <button type='submit' />
+        <button className="btn btn-primary" style={{margin: 20, marginBottom: 50, background: '#B6EAEB', color: 'black', border: 'none'}} type='submit'>Submit</button>
         </p>
       </div>
     )
