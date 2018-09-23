27af09fda21396d7095cef7277a0b140727f53f7
james-built <jgbuilt@gmail.com>
2018-09-23 14:51:46 +1200

    expanded pending reducer to hand create lunchbox requests

--- client/src/reducers/pending.js
+++ client/src/reducers/pending.js
@@ -1,7 +1,11 @@
 import {
   LOGIN_PENDING,
   LOGIN,
-  LOGIN_ERROR } from '../actions'
+  LOGIN_ERROR,
+  CREATE_LUNCHBOX_PENDING,
+  CREATE_LUNCHBOX,
+  CREATE_LUNCHBOX_ERROR
+} from '../actions'
 
 export default function (state = false, action) {
   switch (action.type) {
@@ -10,6 +14,19 @@ export default function (state = false, action) {
     case LOGIN:
       return false
     case LOGIN_ERROR:
+      return false
+    default:
+      return state
+  }
+}
+
+export default function (state = false, action) {
+  switch (action.type) {
+    case CREATE_LUNCHBOX_PENDING:
+      return true
+    case CREATE_LUNCHBOX:
+      return false
+    case CREATE_LUNCHBOX_ERROR:
       return false
     default:
       return state
