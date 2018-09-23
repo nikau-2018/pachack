806e68f7cdc431d5a6e472be37df087aa345bcb3
James Built <jgbuilt@gmail.com>
2018-09-24 11:04:35 +1200

    update lunchboxes table to reference food id
    Former-commit-id: 5a0e726e568cda08040500766b8d0fbae58dbd52

--- server/migrations/20180924105309_update_lunchboxes.js
+++ server/migrations/20180924105309_update_lunchboxes.js
@@ -1,6 +1,10 @@
 exports.up = knex => 
   knex.schema.table('users', t => {
-    
+    t.integer('produce').references('foods.id')
+    t.integer('grains').references('foods.id')
+    t.integer('dairy').references('foods.id')
+    t.integer('protein').references('foods.id')
+    t.integer('other').references('foods.id')
   })
 
 exports.down = knex => knex.schema.dropTable('users')
