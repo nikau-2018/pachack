42c12496d37f365615bfbadaa7e8f0865cec2342
Aimee Coxhead <aimeecoxhead1@gmail.com>
2018-09-24 16:11:46 +1200

    added columns to sign in screen to include intro

--- client/src/components/SignInScreen/index.js
+++ client/src/components/SignInScreen/index.js
@@ -7,7 +7,13 @@ export default class SignInScreen extends Component {
   render () {
     return (
       <div className="container-fluid">
-        <SignIn />
+        <div className="row">
+          <div className="col-md-6"></div>
+          <div className="col-md-6">
+            <SignIn />
+          </div>
+
+        </div>
         <Footer />
       </div>
     )
