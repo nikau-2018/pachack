f4a953e20ef0c12630ddcffaef9ee791cccfbf2e
Lauren Hart <laurenannehart@gmail.com>
2018-09-24 17:34:20 +1200

    add some styling

--- client/src/components/SignInScreen/index.js
+++ client/src/components/SignInScreen/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import Footer from '../Footer'
 import SignIn from '../SignIn'
+import logo from '../Header/images/lb_logo.png'
+import kids from './lunchbunchers.png'
 
 export default class SignInScreen extends Component {
   render () {
@@ -10,11 +12,17 @@ export default class SignInScreen extends Component {
       <div className="container-fluid">
         <div className="row">
           <div className="col-md-6 jumbotron">
-            <h1 className="display-4">Lunch Bunch</h1>
+            <div className="row">
+              <div className="col-md-6">
+                <h1 className="display-4">Lunch Bunch</h1>
+              </div>
+              <div className="col-md-3">
+                <img src={logo}/>
+              </div>
+            </div>
             <p className="lead">welcome to lunch bunch</p>
             <hr className="my-4"/>
             <p>Create amazing lunches. Sign up to create a fantastic lunch!</p>
-            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
           </div>
 
           <div className="col-md-6">
@@ -22,6 +30,8 @@ export default class SignInScreen extends Component {
           </div>
 
         </div>
+
+        <img src={kids} />
         <Footer />
       </div>
     )
