2549b5c3c3e9abd1d2330e5fbf977d6fe14a878f
Lauren Hart <laurenannehart@gmail.com>
2018-09-25 11:37:14 +1200

    styling signin page
    Former-commit-id: ee568f8076b48d81d3708d32d7cb33649b4274d8

--- client/src/components/SignInScreen/index.js
+++ client/src/components/SignInScreen/index.js
@@ -2,37 +2,30 @@ import React, { Component } from 'react'
 
 import Footer from '../Footer'
 import SignIn from '../SignIn'
-import logo from '../Header/images/lb_logo.png'
-import kids from './lunchbunchers.png'
+import logo from '../SignIn/logo.png'
+import apple from '../SignIn/apple.png'
 
 export default class SignInScreen extends Component {
   render () {
     return (
+      <div className="bg">
+        <div className="row signin">
 
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-6 jumbotron">
-            <div className="row">
-              <div className="col-md-6">
-                <h1 className="display-4">Lunch Bunch</h1>
-              </div>
-              <div className="col-md-3">
-                <img src={logo}/>
-              </div>
-            </div>
-            <p className="lead">welcome to lunch bunch</p>
-            <hr className="my-4"/>
-            <p>Create amazing lunches. Sign up to create a fantastic lunch!</p>
+          <div className="col-md-6">
+            <img className='logo' src= {logo}/>
           </div>
 
           <div className="col-md-6">
-            <SignIn />
+            <div className="form">
+              <img className='apple' src= {apple}/>
+              <h4>Welcome to Lunch Bunch</h4>
+              <p>Create amazing lunches.<br/>
+            Sign In or Sign Up ro get started.</p>
+              <SignIn />
+            </div>
           </div>
 
         </div>
-
-        <img src={kids} />
-        <Footer />
       </div>
     )
   }
