90625121987f3675e8f6dad6d7446b38ffacac00
James Built <jgbuilt@gmail.com>
2018-09-24 12:02:10 +1200

    comment out update migrations sqlite limitation, edit original migrations instead
    Former-commit-id: 19b1acec9ea0f6d83a888fdd822c81718e7e3f7c

--- server/migrations/20180924105256_update_foods.js
+++ server/migrations/20180924105256_update_foods.js
@@ -1,6 +1,6 @@
-exports.up = knex =>
-  knex.schema.table('foods', t => {
-    t.timestamps(true, true)
-  })
+// exports.up = knex =>
+//   knex.schema.table('foods', t => {
+//     t.timestamps(true)
+//   })
 
-exports.down = knex => knex.schema.table.dropTimestamps()
+// exports.down = knex => knex.schema.table('foods', t => t.dropTimestamps())
