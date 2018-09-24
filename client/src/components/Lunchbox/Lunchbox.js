7878db2029c3066e392ef446a7e784312fad9861
James Built <jgbuilt@gmail.com>
2018-09-24 14:17:51 +1200

    rendered instances of food component in lunchbox
    Former-commit-id: 67ffcdb47525c2b8c9a5267ad84aa0dcde5fcbd7

--- client/src/components/Lunchbox/Lunchbox.js
+++ client/src/components/Lunchbox/Lunchbox.js
@@ -19,7 +19,6 @@ export default class Lunchbox extends Component {
 
     return (
       <div className='lunchbox'>
-
         <div className='row top'>
           {produce.map((item) => {
             return <Food
@@ -30,63 +29,45 @@ export default class Lunchbox extends Component {
               currentBox={currentBox}
             />
           })}
-
-          <div className='col-md-6 compartment' >
-            <div className='produce'>
-              <div className='producebutton'>
-                <button>Cucumber</button>
-              </div>
-              <div className='currentstate-produce'>
-                {currentBox.lunchbox ? currentBox.lunchbox.produce : <p>empty</p>}
-              </div>
-            </div>
-          </div>
-
-          <div className='col-md-6 compartment'>
-            <div className='grains'>
-              <div className='grainsbutton'>
-                <button>Wheat</button>
-              </div>
-              <div className='currentstate-grains'>
-                {currentBox.lunchbox ? currentBox.lunchbox.grains : <p>empty</p>}
-              </div>
-            </div>
-          </div>
+          {grains.map((item) => {
+            return <Food
+              key={item.id}
+              className={'col-md-6 compartment'}
+              category={'grains'}
+              name={item.name}
+              currentBox={currentBox}
+            />
+          })}
         </div>
 
         <div className='row'>
-          <div className='col-md-4 compartment'>
-            <div className='dairy'>
-              <div className='dairybutton'>
-                <button>Milk</button>
-              </div>
-              <div className='currentstate-dairy'>
-                {currentBox.lunchbox ? currentBox.lunchbox.dairy : <p>empty</p>}
-              </div>
-            </div>
-          </div>
-
-          <div className='col-md-4 compartment'>
-            <div className='protein'>
-              <div className='proteinbutton'>
-                <button>Chicken</button>
-              </div>
-              <div className='currentstate-protein'>
-                {currentBox.lunchbox ? currentBox.lunchbox.protein : <p>empty</p>}
-              </div>
-            </div>
-          </div>
-
-          <div className='col-md-4 compartment'>
-            <div className='other'>
-              <div className='otherbutton'>
-                <button>Chocolate</button>
-              </div>
-              <div className='currentstate-other'>
-                {currentBox.lunchbox ? currentBox.lunchbox.other : <p>empty</p>}
-              </div>
-            </div>
-          </div>
+          {dairy.map((item) => {
+            return <Food
+              key={item.id}
+              className={'col-md-4 compartment'}
+              category={'dairy'}
+              name={item.name}
+              currentBox={currentBox}
+            />
+          })}
+          {protein.map((item) => {
+            return <Food
+              key={item.id}
+              className={'col-md-4 compartment'}
+              category={'protein'}
+              name={item.name}
+              currentBox={currentBox}
+            />
+          })}
+          {other.map((item) => {
+            return <Food
+              key={item.id}
+              className={'col-md-4 compartment'}
+              category={'other'}
+              name={item.name}
+              currentBox={currentBox}
+            />
+          })}
         </div>
       </div>
     )
