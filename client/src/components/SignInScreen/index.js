2b19d999d58a5188dfb3f2d511e64f3d2b73bbde
Lauren Hart <laurenannehart@gmail.com>
2018-09-24 16:29:23 +1200

    jumbotron styling from bootstrap
    Former-commit-id: e3dffba562e6201caf92575f3fcd7a80e0dcb932

--- client/src/components/SignInScreen/index.js
+++ client/src/components/SignInScreen/index.js
@@ -6,9 +6,17 @@ import SignIn from '../SignIn'
 export default class SignInScreen extends Component {
   render () {
     return (
+
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-6"></div>
+          <div className="col-md-6 jumbotron">
+            <h1 className="display-4">Lunch Bunch</h1>
+            <p className="lead">welcome to lunch bunch</p>
+            <hr className="my-4"/>
+            <p>Create amazing lunches. Sign up to create a fantastic lunch!</p>
+            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+          </div>
+
           <div className="col-md-6">
             <SignIn />
           </div>
