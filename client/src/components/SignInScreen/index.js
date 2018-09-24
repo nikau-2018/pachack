1f7841640bb37ad83352392deb1fa9e28a5802eb
Aimee Coxhead <aimeecoxhead1@gmail.com>
2018-09-24 15:59:16 +1200

    removed header from signInScreen index

--- client/src/components/SignInScreen/index.js
+++ client/src/components/SignInScreen/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 
-import Header from '../Header'
 import Footer from '../Footer'
 import SignIn from '../SignIn'
 
@@ -8,7 +7,6 @@ export default class SignInScreen extends Component {
   render () {
     return (
       <div className="container-fluid">
-        <Header />
         <SignIn />
         <Footer />
       </div>
