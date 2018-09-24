8b0a4e6590bb77dbc90b8e4586652c72c2cf2fbc
James Built <jgbuilt@gmail.com>
2018-09-24 15:52:29 +1200

    removed unnecessary div from food component
    Former-commit-id: f516fb737c5c4a409e40eaa64a85ddb9e37f67d5

--- client/src/components/Food/index.js
+++ client/src/components/Food/index.js
@@ -1,15 +1,12 @@
 import React from 'react'
 
 const Food = (props) => (
-  // bootstrap pesentational class
-  <div className={props.className}>
-    <div className={props.category}>
-      <div className={`${props.category}button`}>
-        <button>{props.name}</button>
-      </div>
-      <div className={`currentstate-${props.category}`}>
-        {props.currentBox.lunchbox ? props.currentBox.lunchbox.produce : <p>empty</p>}
-      </div>
+  <div className={props.category}>
+    <div className={`${props.category}button`}>
+      <button>{props.name}</button>
+    </div>
+    <div className={`currentstate-${props.category}`}>
+      {props.currentBox.lunchbox ? props.currentBox.lunchbox.produce : <p>empty</p>}
     </div>
   </div>
 )
