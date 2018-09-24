eb09eee190e1c023b55cf816ad99ee2b03455a54
Rich Churcher <rich.churcher@gmail.com>
2018-09-24 17:58:30 +1200

    Use correct property names
    Former-commit-id: a967f45a589696c49e92e6f3f179bbcd4cdaf876

--- server/db/foods.js
+++ server/db/foods.js
@@ -7,5 +7,10 @@ module.exports = {
 function getFoods (db = connection) {
   return db('foods')
     .join('categories', 'foods.category_id', '=', 'categories.id')
-    .select('categories.name as categoryName', 'categories.id as categoriesId', 'foods.category_id as categoryId', 'foods.name as foodsName', 'foods.id')
+    .select(
+      'categories.name as categoryName',
+      'foods.category_id as categoryId',
+      'foods.name as name',
+      'foods.id as id'
+    )
 }
