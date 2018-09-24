49652b32fb79af6d116a62c709a1383f2fef8d99
James Built <jgbuilt@gmail.com>
2018-09-24 14:29:26 +1200

    added default props on lunchbox
    Former-commit-id: 147ad82d8fc6f68f73dab4b5ea41c7fbc203111f

--- client/src/components/Lunchbox/Lunchbox.js
+++ client/src/components/Lunchbox/Lunchbox.js
@@ -73,3 +73,23 @@ export default class Lunchbox extends Component {
     )
   }
 }
+
+Lunchbox.defaultProps = {
+  foods: {
+    produce: [
+      { id: 66601, name: 'cucumber', categoryId: 1 } 
+    ],
+    grains: [
+      { id: 66618, name: 'almonds', categoryId: 2 } 
+    ],
+    dairy: [
+      { id: 66610, name: 'yoghurt', categoryId: 3 } 
+    ],
+    protein: [
+      { id: 66614, name: 'egg', categoryId: 4 } 
+    ],
+    other: [
+      { id: 66617, name: 'snack_balls', categoryId: 5 } 
+    ]
+  }
+}
