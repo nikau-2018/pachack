9840c7c9988111dc2a92647aa932b75b1d668e0b
Amy Han <syamyhan@gmail.com>
2018-09-25 16:41:19 +1200

    added lunchbox styling

--- client/src/components/Food/index.js
+++ client/src/components/Food/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Food.css'
 
 const Food = (props) => (
   <div className={props.category}>
     <div className={`${props.category}button`}>
-      <button>{props.name}</button>
+      <button className='buttons'>{props.name}</button>
     </div>
   </div>
 )
--- client/src/components/Lunchbox/Lunchbox.js
+++ client/src/components/Lunchbox/Lunchbox.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import './Lunchbox.css'
 import Food from '../Food'
+import logo from '../SignIn/logo.png'
 
 export default class Lunchbox extends Component {
   componentDidMount () {
@@ -19,59 +20,65 @@ export default class Lunchbox extends Component {
     const { foods: { produce, protein, dairy, grains, other }, pending } = foods
 
     return (
-      <div className='lunchbox'>
-        <div className='row top'>
-          <div className='col-md-6 container-fluid produce1'>
-            {produce.map((item) => {
-              return <Food
-                key={item.id}
-                category={'produce'}
-                name={item.name}
-                currentBox={currentBox}
-              />
-            })}
-          </div>
-          <div className='col-md-6 container-fluid grains1'>
-            {grains.map((item) => {
-              return <Food
-                key={item.id}
-                category={'grains'}
-                name={item.name}
-                currentBox={currentBox}
-              />
-            })}
+      <div>
+        <img className='logo2' src= {logo}/>
+        <h1>Your Lunchbox</h1>
+
+        <div className='lunchbox'>
+          <div className='row top'>
+            <div className='col-md-6 container-fluid produce1'>
+              {produce.map((item) => {
+                return <Food
+                  key={item.id}
+                  category={'produce'}
+                  name={item.name}
+                  currentBox={currentBox}
+                />
+              })}
+            </div>
+            <div className='col-md-6 container-fluid grains1'>
+              {grains.map((item) => {
+                return <Food
+                  key={item.id}
+                  category={'grains'}
+                  name={item.name}
+                  currentBox={currentBox}
+                />
+              })}
+            </div>
           </div>
 
-        <div className='row'>
-          <div className='col-md-4 container-fluid dairy1'>
-            {dairy.map((item) => {
-              return <Food
-                key={item.id}
-                category={'dairy'}
-                name={item.name}
-                currentBox={currentBox}
-              />
-            })}
-          </div>
-          <div className='col-md-4 container-fluid protein1'>
-            {protein.map((item) => {
-              return <Food
-                key={item.id}
-                category={'protein'}
-                name={item.name}
-                currentBox={currentBox}
-              />
-            })}
-          </div>
-          <div className='col-md-4 container-fluid other1'>
-            {other.map((item) => {
-              return <Food
-                key={item.id}
-                category={'other'}
-                name={item.name}
-                currentBox={currentBox}
-              />
-            })}
+          <div className='row bottom'>
+            <div className='col-md-4 container-fluid dairy1'>
+              {dairy.map((item) => {
+                return <Food
+                  key={item.id}
+                  category={'dairy'}
+                  name={item.name}
+                  currentBox={currentBox}
+                />
+              })}
+            </div>
+            <div className='col-md-4 container-fluid protein1'>
+              {protein.map((item) => {
+                return <Food
+                  key={item.id}
+                  category={'protein'}
+                  name={item.name}
+                  currentBox={currentBox}
+                />
+              })}
+            </div>
+            <div className='col-md-4 container-fluid other1'>
+              {other.map((item) => {
+                return <Food
+                  key={item.id}
+                  category={'other'}
+                  name={item.name}
+                  currentBox={currentBox}
+                />
+              })}
+            </div>
           </div>
         </div>
       </div>
