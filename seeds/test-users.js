a1377ccae57119c904367f7c30f329074237f454
Rich Churcher <rich.churcher@gmail.com>
2018-10-29 11:47:45 +1300

    Single test user only

--- seeds/test-users.js
+++ seeds/test-users.js
@@ -4,7 +4,6 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        { id: 88801, username: 'rose', hash: '$argon2id$v=19$m=8,t=2,p=1$yjmQK3uZ0b7oK7RqIua5Kw$rciYEEnFUlkunk09KIGN1VRVhAzWpXrDbHN+kfcUqjU', age: 8 },
         { id: 88801, username: 'test', hash: '$argon2id$v=19$m=8,t=2,p=1$yjmQK3uZ0b7oK7RqIua5Kw$rciYEEnFUlkunk09KIGN1VRVhAzWpXrDbHN+kfcUqjU', age: 10 }
       ])
     })
